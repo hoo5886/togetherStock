f3b367d3b5f5ceec3bd4a7787e02bdae0f42e8ec
hoo5886 <hoo5886@daum.net>
2023-03-06 20:57:06 +0900

    feat(jwt를 통한 로그인 구현): jwt를 통한 로그인 구현
    1. jwt 의존성 추가
    2. login controller 수정.
    3. loginDto 등 jwt를 위한 클래스 생성,
    member 도메인 등 수정 등.

--- src/main/java/com/stock/togetherStock/security/SecurityConfig.java
+++ src/main/java/com/stock/togetherStock/security/SecurityConfig.java
@@ -1,26 +1,39 @@
 package com.stock.togetherStock.security;
 
-import com.stock.togetherStock.member.repository.MemberRepository;
+import com.stock.togetherStock.jwt.JwtSecurityConfig;
+import com.stock.togetherStock.jwt.TokenProvider;
+import com.stock.togetherStock.jwt.JwtAuthenticationEntryPoint;
+import com.stock.togetherStock.jwt.JwtAccessDeniedHandler;
 import com.stock.togetherStock.security.handler.AuthFailureHandler;
 import com.stock.togetherStock.security.handler.AuthSuccessHandler;
 import lombok.RequiredArgsConstructor;
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-
     private final AuthSuccessHandler authSuccessHandler;
     private final AuthFailureHandler failureHandler;
+    private final TokenProvider tokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     AuthenticationManager authenticationManager(
@@ -32,16 +45,28 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling()
+            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .accessDeniedHandler(jwtAccessDeniedHandler)
+
+            .and()
             .authorizeRequests()
-            .antMatchers("/post/list").permitAll()
-            .antMatchers("/post/**").authenticated();
-            //.antMatchers("/post/{postId}/comment/write").authenticated()
+            .antMatchers("/post/list", "/login/**", "/", "/member/signup").permitAll()
+            .anyRequest().authenticated()
+
+            .and()
+            .apply(new JwtSecurityConfig(tokenProvider));
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.formLogin()
             .usernameParameter("email")
             .passwordParameter("password")
             .loginPage("/")
-            .loginProcessingUrl("/loginProc")
+            .loginProcessingUrl("/login/authenticate")
             .successHandler(authSuccessHandler)
             .failureHandler(failureHandler);
 
